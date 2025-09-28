70dd56256d55b1a1bce5e4c1350f9d84a0e8f726
RenggoPandora <renggopandora07@gmail.com>
2025-09-28 23:30:34 +0700

    Tugas 3 Pertemuan 4

--- app/src/main/java/com/example/ifunsoedmobile/data/model/SearchResponse.kt
+++ app/src/main/java/com/example/ifunsoedmobile/data/model/SearchResponse.kt
@@ -1,3 +1,17 @@
 package com.example.ifunsoedmobile.data.model
 
-data class SearchResponse()
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName("docs")
+    val docs: List<BookDoc>
+)
+
+data class BookDoc(
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("author_name")
+    val authorName: List<String>,
+    @SerializedName("first_publish_year")
+    val firstPublishYear: Int
+)
